17944d0fb5cf1c182945ad6567f076f9a5a90f7c
yuemengrui <1401190335@qq.com>
2024-01-09 16:55:18 +0800

    add healthcheck

--- AI_Tools_Servers/info/modules/__init__.py
+++ AI_Tools_Servers/info/modules/__init__.py
@@ -1,8 +1,9 @@
 # *_*coding:utf-8 *_*
 from fastapi import FastAPI
-from . import layout, doc_loader
+from . import layout, doc_loader, health
 
 
 def register_router(app: FastAPI):
     app.include_router(router=layout.router, prefix="", tags=["Layout"])
     app.include_router(router=doc_loader.router, prefix="", tags=["Doc Loader"])
+    app.include_router(router=health.router, prefix="", tags=["health"])
